932e6e0a4abc63d9b9bb46a8c036667336c087fc
alexander-slavchev <a.slavchev19.48@gmail.com>
2023-09-23 23:52:50 +0300

    new migration

--- Data/TopRecepti.Data/Migrations/20230923205146_ImageChanges.Designer.cs
+++ Data/TopRecepti.Data/Migrations/20230923205146_ImageChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TopRecepti.Data;
 
@@ -11,9 +12,11 @@ using TopRecepti.Data;
 namespace TopRecepti.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230923205146_ImageChanges")]
+    partial class ImageChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/TopRecepti.Data/Migrations/20230923205146_ImageChanges.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TopRecepti.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RemoteImageUrl",
+                table: "Images",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RemoteImageUrl",
+                table: "Images");
+        }
+    }
+}
